1a86e4d07504d3db7ced0c0205f3d1382892a214
Nyokki <gregory.miyazawa@gmail.com>
2026-02-17 14:51:30 -0500

    feat(server): Added LinkingContext for server

---
+++ src/server/include/LinkingContext.h
@@ -0,0 +1,34 @@
+﻿//
+// LinkingContext.h
+//
+
+#ifndef GODOTPP_LINKINGCONTEXT_H
+#define GODOTPP_LINKINGCONTEXT_H
+#include "entt/entt.hpp"
+
+typedef std::function<entt::entity(entt::registry&)> CreationLambda;
+
+class LinkingContext {
+
+private:
+    struct ObjectContext {
+        uint32_t NetworkId;
+        entt::entity local_entity;
+    };
+
+    std::unordered_map<uint32_t, ObjectContext> network_to_local_map;
+    std::unordered_map<uint32_t, CreationLambda> type_map;
+
+    uint32_t IdCounter = 100;
+
+public:
+    // Objects
+    ObjectContext GetFromNetwork(uint32_t NetworkId);
+
+    // Type
+    void RegisterType(uint32_t TypeId, const CreationLambda &CreatorFunc);
+    ObjectContext CreateFromTypeId(uint32_t TypeId, entt::registry& reg);
+};
+
+
+#endif //GODOTPP_LINKINGCONTEXT_H
---
+++ src/server/src/LinkingContext.cpp
@@ -0,0 +1,29 @@
+﻿//
+// LinkingContext.cpp
+//
+
+#include "../include/LinkingContext.h"
+
+#include <iostream>
+
+LinkingContext::ObjectContext LinkingContext::GetFromNetwork(const uint32_t NetworkId) {
+    if (network_to_local_map.find(NetworkId) != network_to_local_map.end()) {
+        return network_to_local_map[NetworkId];
+    }
+    std::cerr << "Failed to find entity with ID" << NetworkId << std::endl;
+    return {0, entt::null};
+}
+
+void LinkingContext::RegisterType(const uint32_t TypeId, const CreationLambda &CreatorFunc) {
+    type_map[TypeId] = CreatorFunc;
+}
+
+LinkingContext::ObjectContext LinkingContext::CreateFromTypeId(const std::uint32_t TypeId, entt::registry &reg) {
+    if (type_map.find(TypeId) != type_map.end()) {
+        const entt::entity new_entity = type_map[TypeId](reg);
+        const LinkingContext::ObjectContext new_object = {++IdCounter, new_entity};
+        network_to_local_map[IdCounter] = new_object;
+    }
+    std::cerr << "Failed to create entity of type" << TypeId << std::endl;
+    return {0, entt::null};
+}
